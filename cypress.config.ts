253daea1092490abdbb86c922f107b5a744f2d36
Liv-5 <osmith5@kent.edu>
2025-03-10 19:15:44 -0400

    troublehshooting action issue

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'cypress';
-import viteConfig from './vite.config';
+import { defineConfig } from "cypress";
+import viteConfig from "./vite.config";
 
 export default defineConfig({
   component: {
     port: 5173,
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
+      framework: "react",
+      bundler: "vite",
       viteConfig,
     },
   },
 
-  e2e: {
-    baseUrl: 'http://localhost:3001',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  // e2e: {
+  //   baseUrl: 'http://localhost:3001',
+  //   setupNodeEvents(on, config) {
+  //     // implement node event listeners here
+  //   },
+  // },
 });
